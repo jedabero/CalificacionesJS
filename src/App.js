fbb73b9336d86de3c069af542c2bac299459ad4d
jedabero <jedabero20@gmail.com>
2017-01-30 13:19:45 -0500

    Mejorado index y root

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // @flow
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Link, IndexLink} from 'react-router';
+
 import './App.css';
 
 class App extends Component {
@@ -8,12 +9,24 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <ul>
+            <li><IndexLink to="/">Home</IndexLink></li>
+            <li><Link to="/grupos">Grupos</Link></li>
+            <li><Link to="/logout">Salir</Link></li>
+          </ul>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <div>
+            <ul>
+              <li>Grupos <span>{0}</span></li>
+              <li>Periodos <span>{0}</span></li>
+              <li>Asignaturas <span>{0}</span></li>
+            </ul>
+            <div>
+              {this.props.children || <Link to="/grupos">Ver Grupos</Link>}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/Root.js
+++ src/Root.js
@@ -1,19 +1,46 @@
 // @flow
-import React, { Component } from 'react';
+import React, {PropTypes} from 'react';
+import {Provider} from 'react-redux';
+import {Router, Route, browserHistory} from 'react-router';
 
-class Root extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div>
-          Root
-        </div>
-        <div>
-          {this.props.children}
-        </div>
-      </div>
-    );
-  }
-}
+import App from './App';
+import './index.css';
+
+const Login = () => (<div>Login</div>);
+const Logout = () => (<div>Logout</div>);
+const Registro = () => (<div>Registro</div>);
+const Grupos = ({children}) => (<div><div>Grupos</div><div>{children}</div></div>);
+const Grupo = ({params, children}) => (<div><div>Grupo #{params.grupoId}</div><div>{children}</div></div>);
+const Periodos = ({params, children}) => (<div><div>Periodos</div><div>{children}</div></div>);
+const Periodo = ({params, children}) => (<div><div>Periodo #{params.periodoId}</div><div>{children}</div></div>);
+const Asignaturas = ({params, children}) => (<div><div>Asignaturas</div><div>{children}</div></div>);
+const Asignatura = ({params, children}) => (<div><div>Asignatura #{params.asignaturaId}</div><div>{children}</div></div>);
+
+const Root = ({store}) => (
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <Route path="grupos" component={Grupos}>
+          <Route path=":grupoId" component={Grupo}>
+            <Route path="periodos" component={Periodos}>
+              <Route path=":periodoId" component={Periodo}>
+                <Route path="asignaturas" component={Asignaturas}>
+                  <Route path=":asignaturaId" component={Asignatura}/>
+                </Route>
+              </Route>
+            </Route>
+          </Route>
+        </Route>
+      </Route>
+      <Route path="login" component={Login}/>
+      <Route path="logout" component={Logout}/>
+      <Route path="register" component={Registro}/>
+    </Router>
+  </Provider>
+);
+
+Root.propTypes = {
+  store: PropTypes.object.isRequired,
+};
 
 export default Root;
